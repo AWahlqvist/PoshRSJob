f9120097a4c747b703ad03b6908f4b0c64743a07
AWahlqvist <anders.wahlqvist@hotmail.com>
2018-10-16 16:27:25 +0200

    Use native cmdlets to write to the streams

--- PoshRSJob/Private/WriteStream.ps1
+++ PoshRSJob/Private/WriteStream.ps1
@@ -14,10 +14,10 @@
             If (($IndividualJob.$Stream))
             {
                 Switch ($Stream) {
-                    "Verbose" { $IndividualJob | Select-Object -ExpandProperty Verbose| Where-Object { $_ } | ForEach-Object { $host.ui.WriteVerboseLine($_)} }                    
-                    "Debug"   { $IndividualJob | Select-Object -ExpandProperty Debug| Where-Object { $_ } | ForEach-Object { $host.ui.WriteDebugLine($_)} }
-                    "Warning" { $IndividualJob | Select-Object -ExpandProperty Warning| Where-Object { $_ } | ForEach-Object { $host.ui.WriteWarningLine($_) } }
-                    "Error"   { $IndividualJob | Select-Object -ExpandProperty Error | ForEach-Object {$host.ui.WriteErrorLine($_)} }
+                    "Verbose" { $IndividualJob | Select-Object -ExpandProperty Verbose| Where-Object { $_ } | ForEach-Object { Write-Verbose -Message $_ } }                    
+                    "Debug"   { $IndividualJob | Select-Object -ExpandProperty Debug| Where-Object { $_ } | ForEach-Object { Write-Debug -Message $_ } }
+                    "Warning" { $IndividualJob | Select-Object -ExpandProperty Warning| Where-Object { $_ } | ForEach-Object { Write-Warning -Message $_ } }
+                    "Error"   { $IndividualJob | Select-Object -ExpandProperty Error | ForEach-Object { Write-Error -Message $_ } }
                     "Output"  { $IndividualJob | Where-Object { $_ } | Select-Object -ExpandProperty Output }
                 }
             }
